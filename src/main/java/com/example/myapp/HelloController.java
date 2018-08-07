bdd9b722bada8db163fd8929eb9a4a5b1824fe09
ryoheimatsumo <ryo828.kgu@gmail.com>
2018-08-08 00:03:23 +0900

    first-commit

---
+++ src/main/java/com/example/myapp/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.myapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public ModelAndView index(ModelAndView mav) {
+        mav.setViewName("index");
+        return mav;
+    }
+
+
+}
